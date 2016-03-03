b38f7ac310386ec345bcc3a0d450f31fd347d222
Dawson Botsford <DawsonBotsford@gmail.com>
2016-03-03 12:49:59 -0700

    add language selection

--- index.js
+++ index.js
@@ -2,28 +2,53 @@
 'use strict';
 const shell = require('shelljs');
 
-var datetime = new Date();
 let commands = {};
 commands.shell = ['uname', 'echo $SHELL', 'echo $TERM', 'echo $TERM_PROGRAM', 'a'];
 commands.node = ['npm -v', 'node --version'];
+commands.ruby = ['ruby --version'];
+commands.go = ['go --version'];
+
 let outputInfo = '[sist](https://github.com/dawsonbotsford/sist) output:\n';
 
 const shellExec = (command) => {
   let executed = shell.exec(command, {silent: true});
   if (executed.code === 0) {
     return `\`${command}\`: ${executed.output}`;
+  } else {
+    return `\`${command}\`: \`error, return code ${executed.code}\`\n`;
   }
 };
 
-module.exports = function () {
-  for (let type in commands) {
-    outputInfo += `\n### ${type}  \n`;
-    commands[type].forEach(command => {
-      const resp = shellExec(command);
-      if (resp !== undefined) {
-        outputInfo += resp;
+module.exports = function (args) {
+  /* user wants a specific language type */
+  if (args.length > 0) {
+    for (let index in args) {
+      const type = args[index];
+      if (Object.keys(commands).indexOf(type) > -1) {
+        outputInfo += `\n### ${type}  \n`;
+        commands[type].forEach(command => {
+          const resp = shellExec(command);
+          if (resp !== undefined) {
+            outputInfo += resp;
+          }
+        });
+      } else {
+        console.log('args NOT is contained in ');
       }
-    });
+      /*
+      */
+    }
+  } else {
+    /* user wants generic sist output of whole system */
+    for (let type in commands) {
+      outputInfo += `\n### ${type}  \n`;
+      commands[type].forEach(command => {
+        const resp = shellExec(command);
+        if (resp !== undefined) {
+          outputInfo += resp;
+        }
+      });
+    }
   }
-  return outputInfo + `\n\nTime created: ${datetime}`;
+  return outputInfo + `\nTime created: ${new Date()}`;
 };
